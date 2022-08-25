8afdcdd261b85b915aba2e5c6c72578b9d7ecb77
banssa <ban8627@gmail.com>
2022-08-25 11:52:19 +0900

    초기 구성 진행중

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,65 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <BlogHeader />
+  <BlogList />
+  <BlogInput />
+  <BlogFooter />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import BlogHeader from '@/components/BlogHeader.vue';
+  import BlogFooter from '@/components/BlogFooter.vue';
+  import BlogList from '@/components/BlogList.vue';
+  import BlogInput from '@/components/BlogInput.vue';
+  export default {
+    components: {
+      BlogHeader,
+      BlogFooter,
+      BlogList,
+      BlogInput
+    },
+    setup() {
+      return {}
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @charset "utf-8";
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&family=Song+Myung&family=Ubuntu:wght@400;500;700&display=swap');
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  ul,
+  li {
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
+    color: #333;
+  }
+
+  img {
+    border: 0;
+    vertical-align: middle;
+  }
+
+  html {
+    font-size: 16px;
+    overflow-x: hidden;
+  }
+
+  body {
+    font-family: 'Ubuntu', 'Noto Sans KR', sans-serif;
+    /* font-family: 'Song Myung', serif; */
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 1.2;
+    letter-spacing: -0.64px;
+    color:#000;
+  }
 </style>
