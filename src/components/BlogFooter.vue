1e0be5e8a497d7439958c919cb09220b682ad2da
banssa <ban8627@gmail.com>
2022-08-29 10:00:24 +0900

    전체목록 삭제기능 추가

--- src/components/BlogFooter.vue
+++ src/components/BlogFooter.vue
@@ -1,13 +1,51 @@
 <template>
-  <h2>Footer</h2>
+  <div class="clear-all-wrap">
+    <span class="clear-all-bt" @click="clearAllMemo">Celar All</span>
+    <span class="copy">Copyright 2022 by JS Ryu</span>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+  export default {
+    setup() {
+      const clearAllMemo = () => {
+        // localStorage 에서 내용 전체 삭제
+        // 추후 DB 연동 예정
+        localStorage.clear();
+      }
+      return {
+        clearAllMemo
+      }
+    }
+  }
 </script>
 
 <style>
+  .clear-all-wrap {
+    position: relative;
+    display: block;
+    width:100%;
+    /* height: 50px; */
+    line-height:50px;
+    background-color: #fff;
+    text-align: center;
+    margin:0 auto;
+    border-radius: 5px;
+  }
 
+  .clear-all-bt {
+    display: inline-block;
+    width:80%;
+    height:50px;
+    cursor: pointer;
+    border:1px solid #f00;
+    border-radius: 5px;
+    margin:10px;
+  }
+
+  .copy {
+    display: inline-block;
+    font-size: 9px;
+    white-space:nowrap;
+  }
 </style>
